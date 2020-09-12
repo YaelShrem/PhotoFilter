d0275c37e271a7360534332cc8d31776f6044182
YaelBusso <y0583239076@gmail.com>
2020-09-13 01:07:58 +0300

    Updates

---
+++ precache-manifest.bc3104ee1a26b56851d3ee649a5f8945.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6766343c68fe4a1851168ffdbf9ecd",
+    "url": "/PhotoFilter/index.html"
+  },
+  {
+    "revision": "f10e818fa45011611766",
+    "url": "/PhotoFilter/static/css/main.2138bef5.chunk.css"
+  },
+  {
+    "revision": "62e72ba577c96c22a765",
+    "url": "/PhotoFilter/static/js/2.4371ab4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PhotoFilter/static/js/2.4371ab4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f10e818fa45011611766",
+    "url": "/PhotoFilter/static/js/main.a1935b7e.chunk.js"
+  },
+  {
+    "revision": "f597e1380d9025cc635a",
+    "url": "/PhotoFilter/static/js/runtime-main.e209ec83.js"
+  },
+  {
+    "revision": "d679ce4e9e1b81c43644e4c3e301eb1f",
+    "url": "/PhotoFilter/static/media/sweety.d679ce4e.jpeg"
+  }
+]);
